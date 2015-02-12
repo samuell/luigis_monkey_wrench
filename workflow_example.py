691a895cd2a69ed236777a9e230df5e9f8a5afa7
Samuel Lampa <samuel.lampa@gmail.com>
2015-02-12 19:45:42 +0100

    Improving the code comments

--- workflow_example.py
+++ workflow_example.py
@@ -1,8 +1,14 @@
 import luigi
-import luigis_monkey_wrench as lmw 
+import luigis_monkey_wrench as lmw
 
+# Yes, we write the workflow definition inside a normal luigi task ...
 class MyWorkFlow(luigi.Task):
+
+    # ... and do this by setting up the dependency graph and (letting the workflow
+    # task depend on it, by) returning the last task in the dependecy graph in the
+	# workflow task's requires() function:
     def requires(self):
+
         # Create tasks by initializing ShellTasks, and giving
         # the shell tasks to execute to the cmd parameter.
         # File names are given in a this special form:
@@ -20,9 +26,11 @@ class MyWorkFlow(luigi.Task):
         # Return the last task in the workflow
         return fooer
 
+
     def output(self):
         # Return a dummy file that will tell that the workflow is finished
         return luigi.LocalTarget('workflow_finished')
+
 
     def run(self):
         # Write some dummy content to dummy file
@@ -30,7 +38,7 @@ class MyWorkFlow(luigi.Task):
             outfile.write('finished')
 
 
-# We make this file into an executable python file, and let luigi take of the running
+# We finally make this file into an executable python file, and let luigi take of the running
 # which will, among many other cool things, mean that we get a nice command line interface
 # generated for us:
 if __name__ == '__main__':

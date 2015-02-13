3de08a1c0639ec1ec8d67f41bc0a43f12e70fb54
Samuel Lampa <samuel.lampa@scilifelab.uu.se>
2015-02-13 12:09:51 +0100

    Added files() convenience method

--- luigis_monkey_wrench/__init__.py
+++ luigis_monkey_wrench/__init__.py
@@ -12,6 +12,9 @@ def shell(cmd):
     random_id = str(random.random())[2:]
     return ShellTask(cmd=cmd, id=random_id)
 
+def files(file_spec):
+    return shell('# ' + file_spec)
+
 # Task classes
 class AFile(luigi.ExternalTask):
     filename = luigi.Parameter()
